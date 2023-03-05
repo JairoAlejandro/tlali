b2af1d0d32b5ce396517c456c372d4b786ca957d
Jairo Alejandro Camacho López <jacl420021@gmail.com>
2023-03-05 12:44:44 -0600

    commit

---
+++ src/js/config.js
@@ -0,0 +1,46 @@
+export const departaments = [
+    {
+        name: 'legal',
+        icon: 'bx bx-bookmark'
+    },
+    {
+        name: 'finanzas',
+        icon: 'bx bx-money'
+    },
+    {
+        name: 'político',
+        icon: 'bx bx-book'
+    },
+    {
+        name: 'voluntarios',
+        icon: 'bx bx-group'
+    },
+    {
+        name: 'marketing',
+        icon: 'bx bx-desktop'
+    },
+    {
+        name: 'salud',
+        icon: 'bx bx-health'
+    },
+    {
+        name: 'deporte',
+        icon: 'bx bx-football'
+    },
+    {
+        name: 'educación',
+        icon: 'bx bx-book'
+    },
+    {
+        name: 'cultura',
+        icon: 'bx bx-arch'
+    },
+    {
+        name: 'ecología',
+        icon: 'bx bxs-tree'
+    },
+    {
+        name: 'bienestar',
+        icon: 'bx bx-smile'
+    }
+]
